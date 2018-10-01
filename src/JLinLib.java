edaeeb664b57e5d0b76aec69047e5a598544ecec
BT_1N3_34@bktech.local <BT_1N3_34@bktech.local>
2018-10-01 14:48:51 -0400

    Initial commit

---
+++ src/JLinLib.java
@@ -0,0 +1,11 @@
+public class JLinLib {
+    public static void main(String[] args) {
+        public static boolean isPalindrome(String word) {
+            String backwards = "";
+
+            for (i = word.length(); i > 1; i--) {
+                backwards +=
+            }
+        }
+    }
+}
