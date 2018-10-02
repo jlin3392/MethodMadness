c9f40415c67950c54897b4c6212747d7a46736d0
jlin3392 <jlin3392@bths.edu>
2018-10-02 14:21:26 -0400

    Update 1

--- src/TestFile.java
+++ src/TestFile.java
@@ -1,2 +1,18 @@
 public class TestFile {
+    String word = "racecar";
+    public static boolean isPalindrome(String word) {
+        String backwards = "";
+
+        for (int i = word.length(); i > 1; i--) {
+            backwards = backwards + backwards.substring(backwards.length() - i, backwards.length());
+        }
+
+        if (word.equals(backwards)) {
+            System.out.println("true");
+            return true;
+        } else {
+            System.out.println("false");
+            return false;
+        }
+    }
 }
