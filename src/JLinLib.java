0035173cf78486c19a3f193f9e843539bac4e213
BT_1N3_34 <jlin3392@bths.edu>
2018-10-03 14:49:35 -0400

    Edit 2 (incomplete)

--- src/JLinLib.java
+++ src/JLinLib.java
@@ -1,17 +1,13 @@
 public class JLinLib {
-    public static boolean isPalindrome(String word) {
+    public static String isPalindrome(String word) {
         String backwards = "";
-        String chara = "";
 
-        for (int i = word.length(); i > 1; i--) {
-            chara = word.substring(word.length() - i, word.length() - i + 1);
+        for (int i = str.length() - 1) {
+            chara = word.substring
             backwards = backwards + chara;
         }
 
-        if (word.equals(backwards)) {
-            return true;
-        } else {
-            return false;
-        }
+        return backwards;
     }
+
 }
--- src/TestFile.java
+++ src/TestFile.java
@@ -3,7 +3,7 @@ public class TestFile {
     public static void main(String[] args) {
 
         System.out.println("Hello");
-        System.out.println(JLinLib.isPalindrome("racecar"));
+        System.out.println(JLinLib.isPalindrome("pase"));
 
     }
 
